b594f8ae000fbb0173c43aac9774f180fe76b594
mkkm121 <maciej.kolodziej@student.wat.edu.pl>
2022-12-02 13:42:21 +0100

    Fix 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+
+
 function App() {
     return (
+        <>
+      {(function(l) {
+        if (l.search[1] === '/' ) {
+          var decoded = l.search.slice(1).split('&').map(function(s) { 
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(null, null,
+              l.pathname.slice(0, -1) + decoded + l.hash
+          );
+        }
+      }(window.location))}
         <div className="App">
         </div>
+    </>
     );
 }
 
